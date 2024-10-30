b550139f219501ed09e5c8c6d0131bbc3cba64b1
Shakil_xBCC <shakilmrf8@gmail.com>
2024-10-30 19:11:40 +0600

    Updated with logging

--- app/endpoints/router.py
+++ app/endpoints/router.py
@@ -1,7 +1,8 @@
 from pydantic import BaseModel
 from fastapi import APIRouter
 from typing import Dict, Union
-
+from fastapi import HTTPException
+from app.logging import logger
 from app.rag.llm import get_retrieval_chain
 
 qa_router = APIRouter(
@@ -18,8 +19,15 @@ class Query(BaseModel):
 
 @qa_router.post("/question", response_model=Union[str, Dict])
 def answer_question(query: Query):
-    output = chain.invoke({"input": query.input})
-    if query.detailed:
-        return output
-    else:
-        return output["answer"]
+    try:
+        output = chain.invoke({"input": query.input})
+        if query.detailed:
+            return output
+        else:
+            return output["answer"]
+    except Exception as e:
+        logger.warning(f"Error processing question: {str(e)}")
+        raise HTTPException(
+            status_code=500,
+            detail="An error occurred while processing your question"
+        )
